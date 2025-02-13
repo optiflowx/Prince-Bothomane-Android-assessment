3f6fe8045b7e55801917672b6b94fd2f915fd106
Prince B(Opti) <prbothojhb@student.wethinkcode.co.za>
2025-02-13 11:16:06 +0200

    Feat(Logic): Add the profileImageUri to store the uri from storage. For the purpose of this app, this works but in a real world project, we may not want to store the image as temporary data.

--- app/src/main/java/com/glucode/about_you/engineers/models/Engineer.kt
+++ app/src/main/java/com/glucode/about_you/engineers/models/Engineer.kt
@@ -1,9 +1,17 @@
 package com.glucode.about_you.engineers.models
 
+import android.net.Uri
+
 data class Engineer(
     val name: String,
     val role: String,
-    val defaultImageName: String,
+    private var _profileImageUri: Uri? = null,
     val quickStats: QuickStats,
     val questions: List<Question>,
-)
+) {
+    var profileImageUri: Uri?
+        get() = _profileImageUri
+        set(value) {
+            _profileImageUri = value
+        }
+}

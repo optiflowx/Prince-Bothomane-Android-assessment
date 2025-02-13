e82ff13bc9193737dd118da713f5e7266121b998
Prince B(Opti) <prbothojhb@student.wethinkcode.co.za>
2025-02-13 11:27:22 +0200

    Chore(Logic): Parcelize the QuickStats object

--- app/src/main/java/com/glucode/about_you/engineers/models/QuickStats.kt
+++ app/src/main/java/com/glucode/about_you/engineers/models/QuickStats.kt
@@ -1,7 +1,11 @@
 package com.glucode.about_you.engineers.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class QuickStats(
     val years: Int,
     val coffees: Int,
     val bugs: Int
-)
+) : Parcelable

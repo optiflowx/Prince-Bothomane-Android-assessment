5e439f8d1ede8d61bede7591d2e44ebe7793ce01
Prince B(Opti) <prbothojhb@student.wethinkcode.co.za>
2025-02-13 09:34:42 +0200

    Chore: Update MockData.kt
    - Remove unused 'defaultImageName' parameter

--- app/src/main/java/com/glucode/about_you/mockdata/MockData.kt
+++ app/src/main/java/com/glucode/about_you/mockdata/MockData.kt
@@ -10,7 +10,6 @@ object MockData {
         Engineer(
             name = "Reenen",
             role = "Dev manager",
-            defaultImageName = "",
             quickStats = QuickStats(years = 6, coffees = 5400, bugs = 1800),
             questions = listOf(
                 Question.One(Answer(text = "6am", index = 0)),
@@ -23,7 +22,6 @@ object MockData {
         Engineer(
             name = "Wilmar",
             role = "Head of Engineering",
-            defaultImageName = "",
             quickStats = QuickStats(years = 15, coffees = 4000, bugs = 4000),
             questions = listOf(
                 Question.One(Answer(text = "midnight", index = 3)),
@@ -36,7 +34,6 @@ object MockData {
         Engineer(
             name = "Eben",
             role = "Head of Testing",
-            defaultImageName = "",
             quickStats = QuickStats(years = 14, coffees = 1000, bugs = 100),
             questions = listOf(
                 Question.One(Answer(text = "midnight", index = 3)),
@@ -49,7 +46,6 @@ object MockData {
         Engineer(
             name = "Stefan",
             role = "Senior dev",
-            defaultImageName = "",
             quickStats = QuickStats(years = 7, coffees = 9000, bugs = 700),
             questions = listOf(
                 Question.One(Answer(text = "6am", index = 0)),
@@ -62,7 +58,6 @@ object MockData {
         Engineer(
             name = "Brandon",
             role = "Senior dev",
-            defaultImageName = "",
             quickStats = QuickStats(years = 9, coffees = 99999, bugs = 99999),
             questions = listOf(
                 Question.One(Answer(text = "6am", index = 0)),
@@ -75,7 +70,6 @@ object MockData {
         Engineer(
             name = "Henri",
             role = "Senior dev",
-            defaultImageName = "",
             quickStats = QuickStats(years = 10, coffees = 1800, bugs = 1000),
             questions = listOf(
                 Question.One(Answer(text = "6am", index = 0)),

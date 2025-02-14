af3c6d3c3aead9bf2610a8b7128448511ff88c29
Prince Bothomane <princebothomane@gmail.com>
2025-02-14 21:15:30 +0200

    Chore: Revert to default Engineer.kt and navigation_main.xml
    Signed-off-by: Prince Bothomane <princebothomane@gmail.com>

--- app/src/main/java/com/glucode/about_you/engineers/models/Engineer.kt
+++ app/src/main/java/com/glucode/about_you/engineers/models/Engineer.kt
@@ -1,17 +1,9 @@
 package com.glucode.about_you.engineers.models
 
-import android.net.Uri
-
 data class Engineer(
     val name: String,
     val role: String,
-    private var _defaultImageName: Uri? = null,
+    val defaultImageName: String = "",
     val quickStats: QuickStats,
     val questions: List<Question>,
-) {
-    var defaultImageName: Uri?
-        get() = _defaultImageName
-        set(value) {
-            _defaultImageName = value
-        }
-}
+)
